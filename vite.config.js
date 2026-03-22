3a4a6c87b4702958c09331794585215529fa5af8
Hridoy <gazihridoy2006@gmail.com>
2026-03-22 07:09:40 +0600

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/First-diploying-2'
+  base: '/First-diploying',
+  base:process.env.VITE_BASE_PATH || '/First-diploying',
 })
